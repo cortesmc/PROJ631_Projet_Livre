00fd0929e4da9b905b632550979ded7106746260
cortesmc <carlosandres149@gmail.com>
2023-05-04 16:32:19 +0200

    GUI JAVA

---
+++ JAVA/JAVA_Livre/src/MainManagementGUI.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+public class MainManagementGUI extends JFrame implements ActionListener {
+
+    private JLabel titleLabel;
+    private JButton addButton, removeButton, updateButton;
+
+    private ArrayList<String> books;
+
+    public MainManagementGUI(ArrayList<String> books) {
+        super("Library");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+
+        this.books = books;
+
+        // create the title label
+        titleLabel = new JLabel("Library Management", SwingConstants.CENTER);
+
+        // create the add, remove, and update buttons
+        addButton = new JButton("Add Book");
+        removeButton = new JButton("Remove Book");
+        updateButton = new JButton("Update Books");
+
+        // add action listeners to the buttons
+        addButton.addActionListener(this);
+        removeButton.addActionListener(this);
+        updateButton.addActionListener(this);
+
+        // create a panel to hold the buttons
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1, 3));
+        buttonPanel.add(addButton);
+        buttonPanel.add(removeButton);
+        buttonPanel.add(updateButton);
+
+        // add the components to the frame
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(titleLabel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+        add(mainPanel);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addButton) {
+            new addLivreGUI(this.books);
+            this.dispose();
+        } else if (e.getSource() == removeButton) {
+            new removeLivreGUI(this.books);
+            this.dispose();
+
+        } else if (e.getSource() == updateButton) {
+            // handle update books button click
+            // ...
+        }
+    }
+
+}
--- JAVA/JAVA_Livre/src/addLivreGUI.java
+++ JAVA/JAVA_Livre/src/addLivreGUI.java
@@ -2,34 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
+import java.util.ArrayList;
 
-public class GUIaddLivre extends JFrame implements ActionListener {
+public class addLivreGUI extends JFrame implements ActionListener {
 
-    private JTextField titleField, genreField, authorField;
-    private JButton addButton;
+    private JTextField titleField, genreField, authorField,descriptionField;
+    private JButton addButton, goBack;
 
-    public GUIaddLivre() {
+    private ArrayList<String> books;
+
+    public addLivreGUI(ArrayList<String> books) {
         super("Add Book");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 200);
+
+        this.books = books;
 
         // create the text fields and button
         titleField = new JTextField(20);
         genreField = new JTextField(20);
         authorField = new JTextField(20);
+        descriptionField = new JTextField(100);
         addButton = new JButton("Add Book");
         addButton.addActionListener(this);
+        goBack = new JButton("Back");
+        goBack.addActionListener(this);
 
         // add the text fields and button to the frame
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 2));
+        panel.setLayout(new GridLayout(5, 2));
         panel.add(new JLabel("Title:"));
         panel.add(titleField);
         panel.add(new JLabel("Genre:"));
         panel.add(genreField);
         panel.add(new JLabel("Author:"));
         panel.add(authorField);
-        panel.add(new JLabel(""));
+        panel.add(new JLabel("Description:"));
+        panel.add(descriptionField);
+        panel.add(goBack);
         panel.add(addButton);
         add(panel);
 
@@ -42,7 +52,12 @@ public class GUIaddLivre extends JFrame implements ActionListener {
             String title = titleField.getText();
             String genre = genreField.getText();
             String author = authorField.getText();
-            System.out.println("title : "+title+"\ngenre : "+genre+"\nauthor : "+author+"\n");
+            String description = descriptionField.getText();
+            System.out.println("title : "+title+"\ngenre : "+genre+"\nauthor : "+author+"\nDescription : "+description);
+        }
+        if (e.getSource() == goBack) {
+            new MainManagementGUI(this.books);
+            this.dispose();
         }
     }
 }
