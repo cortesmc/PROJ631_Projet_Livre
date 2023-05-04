1f3b371b4539e968707e2b99019f03f2c9ae3e7d
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-05-04 17:53:01 +0200

    [FIX] fix css header

--- WEB/main_page.php
+++ WEB/main_page.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main_page.css" />
+    <script src="https://kit.fontawesome.com/2556c3713e.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
@@ -20,14 +21,32 @@
                 $page=$_GET["page"];
             }
             $encours[$page] = "encours";
-            
+
+            echo "<div>";
             echo "<div><a href=\"?page=0\" class=\"btn_menu $encours[0]\">Titre</a></div>\n";
-            echo "<div><a href=\"?page=1\" class=\"btn_menu $encours[1]\">Home</a></div>\n";
-            echo "<div><a href=\"?page=2\" class=\"btn_menu $encours[2]\">Genres</a></div> \n";   
-            echo "<div><a href=\"?page=3\" class=\"btn_menu $encours[3]\">Biblio</a></div> \n";   
-            echo "<div><form action='\"?page=4\"'><input type='text'></form></div> \n"; 
-            echo "<div><a href=\"?page=5\" class=\"btn_menu $encours[5]\">Sign in</a></div> \n"; 
-            echo "<div><a href=\"?page=6\" class=\"btn_menu $encours[6]\">sign up</a></div> \n"; 
+            echo "</div>";
+
+            echo "<div>";
+            echo "<div class='cliquable_avec_hover'><a href=\"?page=1\" class=\"btn_menu $encours[1]\">Home</a></div>\n";
+            echo "<div class='cliquable_avec_hover'><a href=\"?page=2\" class=\"btn_menu $encours[2]\">Genres</a></div> \n";   
+            echo "<div class='cliquable_avec_hover'><a href=\"?page=3\" class=\"btn_menu $encours[3]\">Biblio</a></div> \n";  
+            echo "</div>";
+            
+            echo "<div>";
+            echo "<div><form action='\"?page=4\"'>
+                    <input type='text' value='Recherche'>
+                    <button type='submit' name='submit'>
+                        <i class='fa-sharp fa-light fa-magnifying-glass'></i>loupe
+                    </button>
+                    </form>
+                    </div> \n"; 
+            echo "</div>";
+            
+            echo "<div >";
+            echo "<div class='cliquable_avec_hover'><a href=\"?page=5\" class=\"btn_menu $encours[5]\">Sign in</a></div> \n"; 
+            echo "<div class='cliquable_avec_hover'><a href=\"?page=6\" class=\"btn_menu $encours[6]\">sign up</a></div> \n"; 
+            echo "</div>";
+
             ?>
         </div>
     
---
+++ WEB/page_0.php
@@ -0,0 +1,6 @@
+<?php
+  
+  echo "<h2>Bienvenue sur votre site de gestion de mesures</h2>\n";
+
+
+?>
