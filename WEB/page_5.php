739c82a97871c968c5cdf23d5c7e636e92c33f50
lilcdx <113095135+lilcdx@users.noreply.github.com>
2023-05-05 14:14:03 +0200

    [ADD] First draft for page 5 (sign up) +css

--- WEB/page_5.php
+++ WEB/page_5.php
@@ -5,20 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="page_5.css" />
+    <script src="https://kit.fontawesome.com/2556c3713e.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
-<body>
-    <div id="content">
-        <span id="title">Connexion</span>
+<div class = "container">
+<span id="title">Creation d'un compte</span>
         <div>
             <form action="" >
                 <span class="form_el">Username</span>
                 <input type="text" class="form_el">
+                <span class="form_el">Nom</span>
+                <input type="text" class="form_el">
+                <span class="form_el">Prénom</span>
+                <input type="text" class="form_el">
                 <span class="form_el">Mot de passe</span>
                 <input type="password" class="form_el">
-                <button class="form_el">Connexion</button>
+                <button class="form_el">Créer</button>
             </form>
         </div>
-        
-    </div>
-</body>
+</div>
+
