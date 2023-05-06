65bb99fbff721cdcb12cb1543ddc73d289943e54
MathieuFerreira1 <Mathieu.Ferreira@etu.univ-savoie.fr>
2023-05-06 12:40:46 +0200

    [ADD] ajout button pour ajouter à la bibliothéque

--- WEB/page_livre.php
+++ WEB/page_livre.php
@@ -12,11 +12,12 @@
     <?php 
 
         $idBook=$_GET["book"] ;
-        $sql="SELECT * FROM book WHERE idBook='$idBook'";
+        $sql="SELECT * FROM book JOIN iswrite ON book.idBook=iswrite.idBook JOIN author ON iswrite.idAuthor=author.idAuthor WHERE book.idBook='$idBook'";
         $result = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error()."\n".$sql);
         while ($row = mysqli_fetch_assoc($result)){
             $thumbnail=$row["thumbnail"];
             $title=$row["title"];
+            $author=$row["name"];
         }
     ?>
     <?php ?>
@@ -24,11 +25,12 @@
 	<div class="container">
 		<div class="image-container">
             <?php echo"<img src=$thumbnail alt='Image'>";?>
-            <h2>Mettre la note ici</h2>
+            <h2>Mettre ma note ici</h2>
+            <button type='submit' name='submit' class='aujout_liste'>Ajouter à ma Bibliotéque</button>
 		</div>
 		<div class="text-container">
             <?php echo"<H1>$title</H1>";?>
-            <H2>Auteur</H2>
+            <?php echo"<H2>$author</H2>";?>
             <p>Note</p>
             <br>
 			<p>Nam pulvinar justo a enim eleifend, sit amet commodo nulla consectetur. Duis nec tempor justo. Sed consequat consequat nibh, sit amet commodo eros lacinia vel. Aenean vitae elementum velit. Aliquam interdum mi in nunc pharetra eleifend. Aenean eleifend, ex nec faucibus egestas, elit elit blandit elit, in fermentum nisi nibh a diam. Nunc bibendum lectus id erat maximus, eu dictum dolor tincidunt. Nulla commodo auctor erat vel dapibus. Morbi facilisis tincidunt nisi, sed aliquam elit dapibus vel. In hac habitasse platea dictumst. Integer auctor, mauris nec commodo hendrerit, quam turpis sollicitudin est, vel lacinia neque felis eu metus. Nullam consequat mi quis velit pharetra faucibus.</p>
